6287a023caec5c9d1e92790ccbdb0df53379e02d
Alisher <toshmurodov2001@mail.ru>
2023-08-06 10:12:07 +0500

    form

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -5,10 +5,11 @@ import './ButtonMain.css'
 function ContactForm() {
     
     return (
-        <form className='Contact__form' autocomplete="off">
-            <input type="text" placeholder="NAME" />
+        <form className='Contact__form' action="https://api.web3forms.com/submit" method="POST" autocomplete="off">
+            <input type="hidden" name="access_key" value="f4a7629a-570b-4080-bcf2-a7c1065a1810"></input>
+            <input type="text" name='name' placeholder="NAME" />
             <input type="email" placeholder="EMAIL" name="email" autocomplete="off" />
-            <textarea placeholder="MESSAGE" autocorrect="on" />
+            <textarea placeholder="MESSAGE" name='message' autocorrect="on" />
             <button type="submit" className='button-main'>Send Message</button>
         </form>
     )
